da9b31887c4c6cd62b89307d7cd49f9b4bbeb7f9
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-09-18 20:46:48 -0500

    exception handling

---
+++ product-service/src/main/java/org/ecom/productservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package org.ecom.productservice.exception;
+
+import lombok.Builder;
+
+@Builder
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException() {
+        super("Resource not found!");
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}

dd745e7f93d9e6fca6246a192a737daaeefb9014
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-09-25 20:37:28 -0500

    comment test

---
+++ common-utils/src/test/java/org/ecom/commonutils/CommonUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecom.commonutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class CommonUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

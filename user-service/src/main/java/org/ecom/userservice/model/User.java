4197ffa16c3f0fa516b1d48341e38544a050b252
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-09-25 21:58:07 -0500

    user models

---
+++ user-service/src/main/java/org/ecom/userservice/model/User.java
@@ -0,0 +1,229 @@
+package org.ecom.userservice.model;
+
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "username")
+    private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @Size(max = 120)
+    @Column(name = "password")
+    @JsonIgnore
+    private String password;
+
+    private boolean accountNonLocked = true;
+    private boolean accountNonExpired = true;
+    private boolean credentialsNonExpired = true;
+    private boolean enabled = true;
+
+    private LocalDate credentialsExpiryDate;
+    private LocalDate accountExpiryDate;
+
+    private String twoFactorSecret;
+    private boolean isTwoFactorEnabled = false;
+    private String signUpMethod;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE})
+    @JoinColumn(name = "role_id", referencedColumnName = "role_id")
+    @JsonBackReference
+    @ToString.Exclude
+    private Role role;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedDate;
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        return userId != null && userId.equals(((User) o).getUserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    public Long getUserId() {
+        return this.userId;
+    }
+
+    public @NotBlank @Size(max = 20) String getUserName() {
+        return this.userName;
+    }
+
+    public @NotBlank @Size(max = 50) @Email String getEmail() {
+        return this.email;
+    }
+
+    public @Size(max = 120) String getPassword() {
+        return this.password;
+    }
+
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public LocalDate getCredentialsExpiryDate() {
+        return this.credentialsExpiryDate;
+    }
+
+    public LocalDate getAccountExpiryDate() {
+        return this.accountExpiryDate;
+    }
+
+    public String getTwoFactorSecret() {
+        return this.twoFactorSecret;
+    }
+
+    public boolean isTwoFactorEnabled() {
+        return this.isTwoFactorEnabled;
+    }
+
+    public String getSignUpMethod() {
+        return this.signUpMethod;
+    }
+
+    public Role getRole() {
+        return this.role;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return this.createdDate;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return this.updatedDate;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setUserName(@NotBlank @Size(max = 20) String userName) {
+        this.userName = userName;
+    }
+
+    public void setEmail(@NotBlank @Size(max = 50) @Email String email) {
+        this.email = email;
+    }
+
+    @JsonIgnore
+    public void setPassword(@Size(max = 120) String password) {
+        this.password = password;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public void setCredentialsExpiryDate(LocalDate credentialsExpiryDate) {
+        this.credentialsExpiryDate = credentialsExpiryDate;
+    }
+
+    public void setAccountExpiryDate(LocalDate accountExpiryDate) {
+        this.accountExpiryDate = accountExpiryDate;
+    }
+
+    public void setTwoFactorSecret(String twoFactorSecret) {
+        this.twoFactorSecret = twoFactorSecret;
+    }
+
+    public void setTwoFactorEnabled(boolean isTwoFactorEnabled) {
+        this.isTwoFactorEnabled = isTwoFactorEnabled;
+    }
+
+    public void setSignUpMethod(String signUpMethod) {
+        this.signUpMethod = signUpMethod;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String toString() {
+        return "User(userId=" + this.getUserId() + ", userName=" + this.getUserName() + ", email=" + this.getEmail() + ", password=" + this.getPassword() + ", accountNonLocked=" + this.isAccountNonLocked() + ", accountNonExpired=" + this.isAccountNonExpired() + ", credentialsNonExpired=" + this.isCredentialsNonExpired() + ", enabled=" + this.isEnabled() + ", credentialsExpiryDate=" + this.getCredentialsExpiryDate() + ", accountExpiryDate=" + this.getAccountExpiryDate() + ", twoFactorSecret=" + this.getTwoFactorSecret() + ", isTwoFactorEnabled=" + this.isTwoFactorEnabled() + ", signUpMethod=" + this.getSignUpMethod() + ", createdDate=" + this.getCreatedDate() + ", updatedDate=" + this.getUpdatedDate() + ")";
+    }
+}
+
+

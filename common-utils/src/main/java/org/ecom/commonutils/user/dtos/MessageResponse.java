fe5a042e595ff67a17552ecbe7073ed537696a36
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-09-25 21:55:02 -0500

    login dtos

---
+++ common-utils/src/main/java/org/ecom/commonutils/user/dtos/MessageResponse.java
@@ -0,0 +1,17 @@
+package org.ecom.commonutils.user.dtos;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

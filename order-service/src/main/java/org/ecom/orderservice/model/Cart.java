d63a5b01ff6884fbc68a981d11c7bc569e7c68bc
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-09-25 21:54:14 -0500

    order models

---
+++ order-service/src/main/java/org/ecom/orderservice/model/Cart.java
@@ -0,0 +1,31 @@
+package org.ecom.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "cart")
+public class Cart {
+
+    @Id
+    private String cartId;
+
+    private Date createdAt;
+
+    @OneToOne
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+
+
+}
